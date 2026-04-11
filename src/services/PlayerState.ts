4b6954d982119c29d71f953a35b6fadfe382eb67
juemrami <juemrami@gmail.com>
2026-04-11 00:51:56 -0700

    fix selection logic bug

--- src/services/PlayerState.ts
+++ src/services/PlayerState.ts
@@ -13,14 +13,27 @@ export class PlayerGameState extends Context.Service<PlayerGameState>()("spellca
 		const selectionPath = Atom.make([] as Array<Tile>)
 		const tryUpdateSelectionPath = Atom.fn(Effect.fn(function*(tile: Tile, get: Atom.FnContext) {
 			const path = get(selectionPath)
-			if (path.at(-1)?.row === tile.row && path.at(-1)?.col === tile.col) {
+			const tail = path.at(-1)
+			// if no exiting path, select tile
+			if (tail === undefined) {
+				get.set(selectionPath, [tile])
+				return true
+			}
+			// if re-selecting tail tile, unselect it
+			if (tail?.row === tile.row && tail?.col === tile.col) {
 				get.set(selectionPath, path.filter((t) => !(t.row === tile.row && t.col === tile.col)))
 				return true
 			}
-			if (path.length === 0 || areTilesAdjacent(path[path.length - 1], tile)) {
+			// if selecting other previously selected tile, do nothing
+			if (path.some((t) => t.row === tile.row && t.col === tile.col)) {
+				return false
+			}
+			// if selecting a tile adjacent to tail, select it
+			if (areTilesAdjacent(tail, tile)) {
 				get.set(selectionPath, [...path, tile])
 				return true
 			}
+			// otherwise, do nothing
 			return false
 		}))
 		const clearSelectionPath = Atom.fn(Effect.fn(function*(_: void, get: Atom.FnContext) {
