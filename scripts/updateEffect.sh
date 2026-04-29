c11f9edb23005a2698c8267e5843bc1e3b538c56
juemrami <juemrami@gmail.com>
2026-04-29 14:31:51 -0700

    chore: update dependancies

--- scripts/updateEffect.sh
+++ scripts/updateEffect.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 pnpm i effect@beta @effect/atom-solid@latest @effect/platform-browser@beta
-pnpm i -D @effect/language-service@latest @effect/tsgo@latest @typescript/native-preview@latest
+pnpm i -D @effect/language-service@latest @effect/tsgo@latest @typescript/native-preview@latest @effect/vitest@beta
